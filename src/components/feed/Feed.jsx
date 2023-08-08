f3fd598601b1ef37fd5c3ae0ebb6b3d1a366ad54
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2023-08-08 12:46:34 +0600

    share component added

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Share from "../share/Share";
 import "./Feed.css";
 
 const Feed = () => {
-  return <div className="feed">feed</div>;
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
---
+++ src/components/share/Share.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./share.css";
+
+const Share = () => {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop"></div>
+        <div className="shareBottom"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
