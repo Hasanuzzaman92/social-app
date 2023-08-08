e0d9bd978845d543ccaaac10883bf0f9be799172
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2023-08-08 11:06:02 +0600

    top bar completed

---
+++ src/components/topBar/TopBar.jsx
@@ -0,0 +1,52 @@
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import PersonIcon from "@mui/icons-material/Person";
+import SearchIcon from "@mui/icons-material/Search";
+import React from "react";
+import "./TopBar.css";
+
+const TopBar = () => {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">SocialSite</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="topbarSearch">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Home</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <PersonIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">3</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">2</span>
+          </div>
+        </div>
+        <img
+          src="/assests/persons/1.jpg"
+          alt="profile-image"
+          className="topbarImage"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from "../../components/topBar/TopBar";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <TopBar />
+    </div>
+  );
+};
+
+export default Home;
