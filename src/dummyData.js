13b1a3c899be5519fec5867c07565a115fa1d950
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2023-08-21 09:36:29 +0600

    dummy data added

---
+++ src/dummyData.js
@@ -0,0 +1,98 @@
+export const Users = [
+  {
+    id: 1,
+    img: "/assests/persons/1.jpg",
+    name: "Nila Alam",
+  },
+  {
+    id: 2,
+    img: "/assests/persons/2.jpg",
+    name: "Sayed Mursalin",
+  },
+  {
+    id: 3,
+    img: "/assests/persons/3.jpg",
+    name: "Hrithesh Varma",
+  },
+  {
+    id: 4,
+    img: "/assests/persons/4.jpg",
+    name: "Angelina Jelli",
+  },
+  {
+    id: 5,
+    img: "/assests/persons/5.jpg",
+    name: "Miss Ketty",
+  },
+  {
+    id: 6,
+    img: "/assests/persons/6.jpg",
+    name: "Suruj Jaman",
+  },
+  {
+    id: 7,
+    img: "/assests/persons/7.jpg",
+    name: "Adam White",
+  },
+  {
+    id: 8,
+    img: "/assests/persons/8.jpg",
+    name: "John Doe",
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "All the post is very awesome in the environment",
+    photo: "assests/post/post1.png",
+    date: "5 Mins ago",
+    userId: 1,
+    like: 69,
+    comment: 10,
+  },
+  {
+    id: 2,
+    desc: "Too err is human",
+    photo: "assests/post/post2.jpg",
+    date: "15 Mins ago",
+    userId: 2,
+    like: 71,
+    comment: 8,
+  },
+  {
+    id: 3,
+    photo: "assests/post/post3.jpg",
+    date: "25 Mins ago",
+    userId: 3,
+    like: 45,
+    comment: 3,
+  },
+  {
+    id: 4,
+    desc: "All the post is very awesome in the environment",
+    photo: "assests/post/post4.jpg",
+    date: "21 Mins ago",
+    userId: 4,
+    like: 40,
+    comment: 4,
+  },
+  {
+    id: 5,
+    desc: "Something is better than nothing",
+    photo: "assests/post/post5.jpg",
+    date: "12 Mins ago",
+    userId: 5,
+    like: 45,
+    comment: 2,
+  },
+  {
+    id: 6,
+    desc: "Swimming is the best excercise",
+    photo: "assests/post/post6.jpg",
+    date: "12 Mins ago",
+    userId: 6,
+    like: 45,
+    comment: 9,
+  },
+];
