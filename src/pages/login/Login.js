2cdb6f4bad84bd09ce13a5b1f64326af1ff51fa4
Hasanuzzaman92 <hasanuzzamanmethu@gmail.com>
2023-08-22 11:35:16 +0600

    login page working

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -12,7 +12,19 @@ const Login = () => {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox"></div>
+          <div className="loginBox">
+            <input type="email" placeholder="Email" className="loginInput" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="loginInput"
+            />
+            <button className="loginButton">Log In</button>
+            <span className="loginForgot">Forgot password</span>
+            <button className="loginRegisterButton">
+              Create a new account
+            </button>
+          </div>
         </div>
       </div>
     </div>
